cf8148b470b2670da40999f69fb507e58b79ebd4
Leon Vincelj <leonvinc7@gmail.com>
2026-04-07 00:28:46 +0200

    Create vjezba1.php

---
+++ vjezba1.php
@@ -0,0 +1,19 @@
+<?php
+$naslov = "Moj prvi PHP dokument";
+$autor = "Leon Vincelj";
+
+echo "<h1>$naslov</h1>";
+echo "<p>Ovu stranicu izradio/la je <strong>$autor</strong>.</p>";
+echo '<a href="https://www.index.hr" target="_blank">Posjetite Index.hr</a>';
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+</body>
+</html>
